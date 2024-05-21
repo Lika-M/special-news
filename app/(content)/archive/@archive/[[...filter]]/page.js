9ebf8ce29c09e19cdf09e52c8daeecfb51b7dc14
Iglika Mincheva <103664008+Lika-M@users.noreply.github.com>
2024-05-21 15:08:22 +0300

    Refactored code to display a fallback using the Suspense when the component is already rendered and the loading file is not working.

--- app/(content)/archive/@archive/[[...filter]]/page.js
+++ app/(content)/archive/@archive/[[...filter]]/page.js
@@ -1,61 +1,81 @@
-import { getAvailableNewsYears, getAvailableNewsMonths } from "@/util/news.js";
-import { getNewsForYear, getNewsForYearAndMonth } from "@/util/news.js";
+import { Suspense } from "react";
 import NewsList from "@/components/news-list.js";
+import {
+    getAvailableNewsYears, getAvailableNewsMonths,
+    getNewsForYear, getNewsForYearAndMonth
+} from "@/util/news.js";
+
 
 import Link from "next/link.js";
 
-export default async function FilteredNewsPage({ params }) {
-    const filter = params.filter; //array of segments
+async function FilteredHeader({ year, month }) {
+    const availableYears = await getAvailableNewsYears();
+    const availableMonth = getAvailableNewsMonths(year);
 
-    const year = filter?.[0];
-    const month = filter?.[1];
-
-    let news;
-    let links = await getAvailableNewsYears();
-    let contentNews = 'No news found of the selected period.'
+    let links = availableYears;
 
     if (year && !month) {
-        news = await getNewsForYear(year);
         links = getAvailableNewsMonths(year);
     }
 
     if (month) {
-        news = await getNewsForYearAndMonth(year, month);
         links = [];
+    }
+
+
+    if ((year && !availableYears.includes(year)) ||
+        (month && !availableMonth.includes(month))) {
+        throw new Error('Invalid path.');
+    }
+
+    return (
+        <header id="archive-header">
+            <nav>
+                <ul>
+                    {links.map(link => {
+                        const href = !year ? `/archive/${link}` : `/archive/${year}/${link}`;
+
+                        return (
+                            <li key={link}>
+                                <Link href={href}>
+                                    {link}
+                                </Link>
+                            </li>
+                        )
+                    })}
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+async function FilteredNews({ year, month }) {
+    let news;
+    let contentNews = 'No news found of the selected period.';
+
+    if (year && !month) {
+        news = await getNewsForYear(year);
+    } else if (year && month) {
+        news = await getNewsForYearAndMonth(year, month);
     }
 
     if (news && news.length > 0) {
         contentNews = <NewsList news={news} />
     }
 
-    const availableYears = await getAvailableNewsYears();
-    
-    if ((year && !availableYears.includes(year)) ||
-        (month && !getAvailableNewsMonths(year).includes(month)) ||
-        (filter && filter.length > 2)) {
-        throw new Error('Invalid path.');
-    }
+    return contentNews;
+}
+
+export default function FilteredNewsPage({ params }) {
+    const filter = params.filter; //array of segments
+
+    const year = filter?.[0];
+    const month = filter?.[1];
 
     return (
-        <>
-            <header id="archive-header">
-                <nav>
-                    <ul>
-                        {links.map(link => {
-                            const href = !year ? `/archive/${link}` : `/archive/${year}/${link}`;
-
-                            return (
-                                <li key={link}>
-                                    <Link href={href}>
-                                        {link}
-                                    </Link>
-                                </li>
-                            )
-                        })}
-                    </ul>
-                </nav>
-            </header>
-            {contentNews}
-        </>
+        <Suspense fallback={<p>Loading filtered news...</p>}>
+            <FilteredHeader year={year} month={month} />
+            <FilteredNews year={year} month={month} />
+        </Suspense>
     );
 }
