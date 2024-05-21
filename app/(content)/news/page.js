8d4f91cf0b2f953bdf07f8b3f6f37c929b12c08d
Iglika Mincheva <103664008+Lika-M@users.noreply.github.com>
2024-05-21 11:37:55 +0300

    Copied the database from a separate backend server into the root of the Next.js project. Added the 'better-sqlite3' dependency to directly retrieve data using Node.js as the server. Added a loading fallback to the news page.

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,14 +1,9 @@
-import { DUMMY_NEWS } from '@/dummy-news.js';
 import NewsList from "@/components/news-list.js";
+import { getAllNews } from "@/util/news.js";
 
 export default async function NewsPage() {
+    const news = await getAllNews();
 
-    const response = await fetch('http://localhost:8080/news');
-
-    if(!response.ok){
-        throw new Error('Failed to fetch news.');
-    }
-    const news = await response.json();
     return (
         <>
             <h1>News Page</h1>
